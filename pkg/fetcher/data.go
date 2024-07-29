89645ed008b2b8e08c96ed938f2cc64af89cb17e
chechia <chechia@maicoin.com>
2024-07-29 22:48:55 +0800

    fix: date time format to rfc3339

--- pkg/fetcher/data.go
+++ pkg/fetcher/data.go
@@ -13,7 +13,7 @@ func DataToCsv(data []Data) [][]string {
 	csv := make([][]string, 0)
 	for _, d := range data {
 		csv = append(csv, []string{
-			d.Time.Format("2006-01-02 15:04:05"),
+			d.Time.Format(time.RFC3339),
 			d.Name,
 			d.Number,
 			d.Capacity,
